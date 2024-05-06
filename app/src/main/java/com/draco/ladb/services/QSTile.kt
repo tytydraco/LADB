d42bb85a18c7b508f5e6d84dcd2060895cc5e3bc
VaiMalaviya <55515799+vaimalaviya1233@users.noreply.github.com>
2024-05-06 02:39:41 -0500

    updated to latest sdk and changed deprecated code for newer android in QSTile.kt added new option of pending intent for newer android and kept older code for version before UPSIDE_DOWN_CAKE
    there are nine library updated which is not in this commit. didn't update libraries some library might break the build that's why_will update in other pr commit.

--- app/src/main/java/com/draco/ladb/services/QSTile.kt
+++ app/src/main/java/com/draco/ladb/services/QSTile.kt
@@ -1,14 +1,37 @@
 package com.draco.ladb.services
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent
 import android.content.Intent
+import android.os.Build
 import android.service.quicksettings.TileService
 import com.draco.ladb.views.MainActivity
 
 class QSTile : TileService() {
+    @SuppressLint("StartActivityAndCollapseDeprecated")
     override fun onClick() {
         super.onClick()
-        val intent = Intent(applicationContext, MainActivity::class.java)
-            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        startActivityAndCollapse(intent)
+        val intent = Intent(
+            applicationContext,
+            MainActivity::class.java
+        ).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        
+        // code added to not cause avoid deprecated method for newer android
+        // and to avoid UnsupportedOperationException
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            // ---------------------------------REVIEW NEEDED-----------------------------
+            // pretty sure it should be getService call otherwise it should be getActivity
+            PendingIntent.getService(
+                applicationContext,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_ONE_SHOT
+                )
+        }else{
+            // if sdk_int is less than UPSIDE_DOWN_CAKE
+            startActivityAndCollapse(intent)
+        }
+        
     }
 }
