a6978c14f9ebc9562364d4abe1bc88f22657f7a1
Tyler Nijmeh <tylernij@gmail.com>
2025-05-24 17:33:58 -0700

    Support android 14 QS tile intents
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- app/src/main/java/com/draco/ladb/services/QSTile.kt
+++ app/src/main/java/com/draco/ladb/services/QSTile.kt
@@ -1,14 +1,30 @@
 package com.draco.ladb.services
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent
 import android.content.Intent
+import android.os.Build
 import android.service.quicksettings.TileService
 import com.draco.ladb.views.MainActivity
 
 class QSTile : TileService() {
+    @SuppressLint("StartActivityAndCollapseDeprecated")
     override fun onClick() {
         super.onClick()
         val intent = Intent(applicationContext, MainActivity::class.java)
             .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        startActivityAndCollapse(intent)
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            startActivityAndCollapse(
+                PendingIntent.getActivity(
+                    applicationContext,
+                    0,
+                    intent,
+                    PendingIntent.FLAG_IMMUTABLE
+                )
+            )
+        } else {
+            startActivityAndCollapse(intent)
+        }
     }
 }
